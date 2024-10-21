0a2d390d2a52d726e4ec39e619669d1bc90151e3
OdyLiagkas <31992887+OdyLiagkas@users.noreply.github.com>
2024-10-21 13:34:58 +0300

    changed wandb project name

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ epochs = 3 ### on Github they say 200 for the MNIST set
 # start a new wandb run to track this script
 wandb.init(
     # set the wandb project where this run will be logged
-    project="my-awesome-project",
+    project="cryoEM-WGAN",
 
     # track hyperparameters and run metadata
     config={
