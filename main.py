35447401477df94eebb4edc9fbc51a1d5f9c1cfd
OdyLiagkas <31992887+OdyLiagkas@users.noreply.github.com>
2024-10-21 12:11:08 +0300

    epochs =300

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ betas = (beta_1,beta_2)
 
 img_size = (128,128,1)
 
-epochs = 30 ### on Github they say 200 for the MNIST set
+epochs = 300 ### on Github they say 200 for the MNIST set
 ############################################################################################
 data_loader = get_dataloader(batch_size=batch_size)
 
