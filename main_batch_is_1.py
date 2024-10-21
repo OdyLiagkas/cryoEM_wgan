9c0b909c18f0b7c7e8944361b0b7fa47337b3d98
OdyLiagkas <31992887+OdyLiagkas@users.noreply.github.com>
2024-10-21 17:56:07 +0300

    Update main_batch_is_1.py

--- main_batch_is_1.py
+++ main_batch_is_1.py
@@ -32,7 +32,7 @@ wandb.init(
     # set the wandb project where this run will be logged
     project="cryoEM-WGAN",
 
-    name="MNIST_One_Image_Dataset",
+    name="MNIST_One_Image_Dataset_MORE_PLOTS",
     
     # track hyperparameters and run metadata
     config={
