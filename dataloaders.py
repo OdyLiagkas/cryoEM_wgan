b0f2948a97ba65d9cb4daceaf2b48b56ff24d1de
Andreas Zamanos <andreas.zamanos@athenarc.gr>
2024-10-25 17:12:35 +0300

    check

--- dataloaders.py
+++ dataloaders.py
@@ -20,7 +20,7 @@ class CustomImageDataset(Dataset):
                 image = self.transform(image)
             else:
                 image = (np.array(image)/255)[None, :, :]
-            print(image.shape)
+            print(image.min())
             self.particles.append(image)
         self.particles = np.array(self.particles)
         if standarization:
