081cb5bf5278b2548e38c25fd5fc1cc9a3fde071
OdyLiagkas <31992887+OdyLiagkas@users.noreply.github.com>
2025-01-31 12:38:14 +0200

    Update dataloaders.py

--- dataloaders.py
+++ dataloaders.py
@@ -66,3 +66,91 @@ def get_dataloader(paths_to_data, batch_size, standarization=False):
     )
 
     return DataLoader(particle_dset, batch_size=batch_size, shuffle=True, num_workers=8) 
+
+
+
+
+#new from cryospin:
+
+from encoders import GaussianPyramid
+
+class Spin_CustomImageDataset(Dataset):
+    def __init__(self, img_dir, transform=None, target_transform=None, standarization=False, sidelen = 200, num_octaves=4):
+        self.particles = []
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.sidelen = sidelen
+        self.num_octaves = num_octaves
+
+        # Gaussian Pyramid
+        self.gaussian_filters = GaussianPyramid(
+                kernel_size=11,
+                kernel_variance=0.01,
+                num_octaves=num_octaves,
+                octave_scaling=10
+            )
+                
+
+        # List all files in the directory (assuming they are images)
+        self.img_names = sorted(os.listdir(img_dir))
+        for imname in self.img_names:
+            img_path = os.path.join(self.img_dir, imname)  # get specific image path
+            image = Image.open(img_path)
+            if self.transform:
+                image = self.transform(image)
+            else:
+                image = (np.array(image) / 255)[None, :, :]  # Normalize to [0, 1]
+            # Normalize to [-1, 1] if specified
+            #if self.normalize_for_tanh:
+                #image = (image - 0.5) * 2  # Convert [0, 1] to [-1, 1]
+
+
+            image = self.gaussian_filters(image)     #ADDED GAUSSIAN FILTERS
+            
+            self.particles.append(np.array(image))
+            
+        
+        self.particles = np.array(self.particles)
+        
+        # Standardization if specified
+        if standarization:
+            self.particles = (self.particles - self.particles.mean()) / self.particles.std()
+
+    def __len__(self):
+        return len(self.img_names)
+
+    def __getitem__(self, idx):
+        return self.particles[idx]
+
+def get_spin_dataloader(paths_to_data, batch_size, standarization=False):
+    """Loads datasets from the provided list of paths.
+
+    paths_to_data : list
+        A list of paths to dataset directories.
+    batch_size : int
+        The batch size for the dataloader.
+    """
+    transform = transforms.Compose([
+        transforms.Resize(128),
+        transforms.CenterCrop(128),
+        transforms.ToTensor()
+    ])
+    transform = None
+
+    # Use the first path from the list of data paths (can be adjusted if multiple datasets are needed)
+    selected_path = paths_to_data[0]
+
+    # Get dataset
+    particle_dset = Spin_CustomImageDataset(
+        img_dir=selected_path,
+        transform=transform,
+        standarization=standarization
+        
+    )
+
+    return DataLoader(particle_dset, batch_size=batch_size, shuffle=True, num_workers=8) 
+
+
+
